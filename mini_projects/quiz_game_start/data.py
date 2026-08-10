ef462c09accb636fd6b9de7ca3ba74cbe3ff520d
Shruti Kothale <140692609+Shruti0005@users.noreply.github.com>
2026-08-10 11:47:23 +0530

    Create data.py

---
+++ mini_projects/quiz_game_start/data.py
@@ -0,0 +1,42 @@
+question_data = [
+    {
+        "question": "The Windows ME operating system was released in the year 2000.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "The HTML5 standard was published in 2014.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "JavaScript derives from a later version of Java",
+        "correct_answer": "False"
+    },
+    {
+        "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "The first IBM PC was released in 1981.",
+        "correct_answer": "True"
+    },
+    {
+        "question": "The logo for Snapchat is a Bell.",
+        "correct_answer": "False"
+    },
+    {
+        "question": "Linus Torvalds created Linux and Git.",
+        "correct_answer": "True"
+    }
+]
